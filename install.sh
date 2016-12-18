7e75a2c9e5264d44dce7eb6eba69a628517baa72
rfrohl <frohl.robert@gmail.com>
2016-12-18 13:01:20 -0800

    add support for neovim

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/init.vim ~/.vimrc
 ln -s ~/.vim/gvimrc ~/.gvimrc
+
+# neovim
+if [ ! -d  ~/.config ] ; then
+    mkdir ~/.config
+fi
+ln -s ~/.vim ~/.config/nvim
+
 cd ~/.vim && git submodule update --init
 
